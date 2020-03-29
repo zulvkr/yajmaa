7607322da440af4812fd2c6671c1389c04bda9a5
Ivan Zulfikar <ivan.zulf.ikar@gmail.com>
2020-03-29 23:57:19 +0700

    experimenting with vue carousel, make home page

--- _includes/assets/js/app.js
+++ _includes/assets/js/app.js
@@ -12,3 +12,16 @@ var buttonAside = new Vue({
   el: "#fr-button-2",
   data: {}
 });
+
+var carousel = new Vue({
+  el: "#carousel",
+  data: {
+    carousels: [
+      { text: 'Slide 1', color: 'primary' },
+      { text: 'Slide 2', color: 'info' },
+      { text: 'Slide 3', color: 'success' },
+      { text: 'Slide 4', color: 'warning' },
+      { text: 'Slide 5', color: 'danger' }
+    ]
+  }
+});
